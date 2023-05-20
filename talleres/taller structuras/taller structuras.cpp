f2d5e0faabbfb3bad1719e1a84d4272bb2a63997
JesusProgramador2 <avegar@udistrital.edu.co>
2023-05-20 10:13:40 -0600

    version finalv1

--- talleres/taller structuras/taller structuras.cpp
+++ talleres/taller structuras/taller structuras.cpp
@@ -2,41 +2,40 @@
 #include<stdlib.h>
 
 void nuevo(struct datos *);
-
 struct datos
 {
     char nombre[30];
     int salario;
-    
-}*pun;
+};
 
-int n = 1;
+int n = 2;
 
 int main()
 {
     FILE *parch;
-    int i, temp, mayor=0, n;  
-    pun = (struct datos*) malloc(sizeof(struct datos));
+    int i, temp, mayor=0, cont;  
+    struct datos *pun;
+    
     parch = fopen("sueldos.txt","r"); //
+    fscanf(parch, "%*s %*s %*s %i", &cont); //lee el archivo y se salta las 3 cadenas de caracteres que hay al principio para
+    //leer solo el numero de datos que hay en el txt 
+    pun = (struct datos*) malloc(sizeof(struct datos));
     
-    
-    for ( i = 0; i < 10; i++)
+    for ( i = 0; i < cont; i++)
     {
         fscanf(parch, "%s %i",&(pun+i)->nombre, &(pun+i)->salario);
+        nuevo(pun);
         temp = (pun+i)->salario;
         
         if (temp > mayor)
         {
-            
             mayor = (pun+i)->salario;    
         }
-        
     }
     fclose(parch);
-    for ( i = 0; i < 10; i++)
+    for ( i = 0; i < cont; i++)
     {
         printf("lei esto %s %i\n",(pun+i)->nombre,(pun+i)->salario);
-        
     }
     printf("el valor mayor es:%i\n",mayor);
     return 0;
