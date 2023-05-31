8374e8f7c5547a9370cd45109a579830cab76546
JesusProgramador2 <avegar@udistrital.edu.co>
2023-05-30 22:47:29 -0600

    ya casi totalmente terminado

--- talleres/taller finalv1/come galletas.c
+++ talleres/taller finalv1/come galletas.c
@@ -8,31 +8,35 @@ int main()
 {
     FILE *p;
     char tecla, tabla[5][5];
-    int pos[2], puntaje;
-    p = fopen("log.txt","a+");
+    int pos[2], turnos, puntaje;
+    p = fopen("log.txt","a+");//en modo añadir y si no existe lo crea para registrar los turnos 
     
     
     inicio(tabla,pos);
+    turnos = 0;
     
     do
     {
         
         impcon(tabla);
-        impx(tabla, p);
+        impx(tabla, p); // funcion que imprime en consola la tabla que le pases
+         //funcion que imprime en .txt la tabla que le pases 
+        tabla[pos[0]][pos[1]] = '-';//pone un '-' donde estaba la G para que paresca que se mueve
+        movimiento(tabla,pos); // funcion que genero una posicion adyancente aleatoria y la devuelve como arreglo
+        tabla[pos[0]][pos[1]] = 'G'; // asigna la g a la nueva posicion generada por la funcion anterior   
+        turnos++;//contador para saber cuanto te tardaste en completar el juego
+        puntaje = contar(tabla);
+        
         tecla = getch();
-    } while (puntaje == 5 || toascii(tecla) == 27 );
-    
-    
-    
-    
-    
-    
-    printf("buenas");
+    }while(tecla == 'e' && puntaje != 0);
+    impcon(tabla);
+    impx(tabla, p);
+    //printf("%i",turnos);
     
     fclose(p);
     return 0;
 }
-void inicio(char tabla[5][5], int pos[2])
+void inicio(char tabla[5][5], int pos[2])//funcion que llama a funciones que inician el tablero las galletas y el comegalletas
 {
     rellenar(tabla);
     galleta(tabla);
--- talleres/taller finalv1/galletas.h
+++ talleres/taller finalv1/galletas.h
@@ -4,7 +4,8 @@
     #include<stdio.h>
     #include<time.h>
     
-    void impx(char tabla[5][5], FILE *parch)
+    void impx(char tabla[5][5], FILE *parch)//funcion que le entra un puntero y una tabla e imprime de manera ordenada
+    // en un txt la tabla 
     {
         int i, j;
         
@@ -18,7 +19,8 @@
             }
         }   
     }
-    void impcon(char tabla[5][5])
+    void impcon(char tabla[5][5])//funcione que imprime en consola la tabla de manera ordenada
+    //
     {
         int i,j;
         puts("");
@@ -31,7 +33,7 @@
             }
         }
     }
-    void rellenar(char tabla[5][5])
+    void rellenar(char tabla[5][5]) //funcion que rellena la tabla de guiones
     {
         int i, j;
         for ( i = 0; i < 5; i++)
@@ -42,7 +44,8 @@
             }
         }
     }
-    void galleta(char tabla[5][5])
+    void galleta(char tabla[5][5]) //funcion que por medio de aleatorios y condiciones asigna 5 galletas alrededor del tablero
+    //y no se repetin nunca por la condicion siempre va a ver 5 galletas en el tablero
     {
         int i, x, y;
         srand(time(NULL));
@@ -59,7 +62,8 @@
 
         }
     }   
-    void posicion(char tabla[5][5], int posicion[])
+    void posicion(char tabla[5][5], int posicion[]) //funcion que designa la posicion inicial de 'G' por medio de aleatorios
+    //y revisa si es en la posicion de una galleta para nunca asignar esa posicion inicial
     {
         int x, y;
         x = rand() % 5;
@@ -73,22 +77,63 @@
         posicion[0] = x;
         posicion[1] = y;
     }
-    void movimiento(char tabla[5][5], int pos[2])
+    void movimiento(char tabla[5][5], int pos[2])//funcion que da un nuevo valor de posicion adyacente de donde este 'G'
+    // con condiciones para que no se salga del  tablero
     {
-        int x,y, temp;
+        int x, y;
         
-        x = rand() % 2;
-        if (temp == 0)
+        do
         {
-            x = -1;
-        }
-        y = rand() % 2;
-        if (temp == 0)
+            x = rand() % 3;
+            if (x == 0)
+            {
+                x = -1;
+            }
+            if (x == 2)
+            {
+                x == 0;
+            }
+            
+            //puts("llegue hasta aqui x");
+        }while((pos[0] + x) < 0 || (pos[0] + x) > 4);
+            
+        do
         {
-            y = -1;
+            y = rand() % 3;
+            if (y == 0)
+            {
+                y = -1;
+            }
+            if (y == 2)
+            {
+                y = 0;
         }
         
-        
+            //puts("llegue hasta aqui y"); //print que me confirmo que el codigo llego hasta aqui fue util para descubirir un errro
+        } while ((pos[1] + y) < 0 || (pos[1] + y) > 4);
+        pos[0] = pos[0] + x;
+        pos[1] = pos[1] + y;
     }
-
+    int contar(char tabla[5][5]) //funcion que cuenta cuantas galleta quedan en el tablero para saber si agarraste una
+    // cuando acabaste el juego
+    {
+        int i, j, cont=0;
+        for (i = 0; i < 5; i++)
+        {
+            for (j = 0; j < 5; j++)
+            {
+                if (tabla[i][j] == 'O')
+                {
+                    printf("buenas");
+                    cont++;
+                    printf("%i",cont);
+                }
+                
+            }
+            
+        }
+        
+        
+        return cont;
+    }
 #endif
